9a6d85d6666c1a3e32a3c571e4b36a65083ace94
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-16 21:55:31 +0000

    NaN is upadated

---
+++ 2.IsNaN.js
@@ -0,0 +1,12 @@
+// The isNaN() function checks if a value is NaN (Not-a-Number) or not.
+
+ let num = isNaN(548);
+
+console.log(num);
+
+// The isNaN() function takes in:
+
+// value - The value to be tested.
+
+// Returns true when the argument is NaN.
+// Returns false for other arguments.
