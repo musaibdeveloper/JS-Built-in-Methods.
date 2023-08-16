b4a34824b27d845845fd533ea88478cd9a2a99c9
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-16 08:54:27 +0000

    JS-BUILT-IN-METHODS

---
+++ 1.isFinte.js
@@ -0,0 +1,30 @@
+// The isFinite() function checks whether the passed value is a finite number or not.
+
+// check whether 5463 is finite or not
+console.log(isFinite(5463));
+
+// Output: true
+
+
+
+console.log(isFinite(NaN)); // False. 
+
+// check whether Infinity is finite or not
+console.log(isFinite(Infinity));
+
+// Output: false
+
+// check whether undefined is finite or not
+console.log(isFinite(undefined));
+
+// Output: false
+
+// check whether NaN is finite or not
+console.log(isFinite(undefined));
+
+// Output: false
+
+// check whether null is finite or not
+console.log(isFinite(null));
+
+// Output: true
