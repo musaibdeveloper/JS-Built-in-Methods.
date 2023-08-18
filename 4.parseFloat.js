fa2877656efd05a8b5f8ca2a8f8673e5486e5ab2
musaibdeveloper <syedmusaib428@gmail.com>
2023-08-18 15:48:38 +0000

    JS-BUILT-IN-METHOD

---
+++ 4.parseFloat.js
@@ -0,0 +1,9 @@
+// The parseFloat() method parses a value as a string and returns the first number.
+
+const stringDate = "23.9";
+
+// parse the string to float value
+let floatDate = parseFloat(stringDate);
+console.log(floatDate)
+
+// Output: 23.9
